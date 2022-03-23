e9074d5cd4f7a7928b635a03f843c39f8132f5c4
Preocts <preocts@gmail.com>
2022-03-22 21:47:31 -0400

    Use __future__ annotations

--- src/http_overeasy/http_client.py
+++ src/http_overeasy/http_client.py
@@ -1,8 +1,8 @@
+from __future__ import annotations
+
 import json
 import logging
 from typing import Any
-from typing import Dict
-from typing import Optional
 from urllib import parse
 
 import urllib3
@@ -22,7 +22,7 @@ class HTTPClient:
     def __init__(
         self,
         *,
-        headers: Optional[Dict[str, str]] = None,
+        headers: dict[str, str] | None = None,
         max_pool: int = 10,
     ) -> None:
         self.log = logging.getLogger(__name__)
@@ -46,8 +46,8 @@ class HTTPClient:
     def get(
         self,
         url: str,
-        fields: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        fields: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """
         GET method with Response model returned
@@ -65,8 +65,8 @@ class HTTPClient:
     def delete(
         self,
         url: str,
-        fields: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        fields: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """
         DELETE method with Response model returned
@@ -84,8 +84,8 @@ class HTTPClient:
     def post(
         self,
         url: str,
-        body: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        body: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """
         POST method with Response model returned
@@ -104,8 +104,8 @@ class HTTPClient:
     def put(
         self,
         url: str,
-        body: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        body: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """
         PUT method with Response model returned
@@ -124,8 +124,8 @@ class HTTPClient:
     def patch(
         self,
         url: str,
-        body: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        body: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """
         PATCH method with Response model returned
@@ -145,8 +145,8 @@ class HTTPClient:
         self,
         method: str,
         url: str,
-        body: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        body: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """Internal: Handles POST, PUT, and PATCH"""
         headers = self._format_headers(headers) if headers is not None else self.headers
@@ -170,8 +170,8 @@ class HTTPClient:
         self,
         method: str,
         url: str,
-        fields: Optional[Dict[str, Any]] = None,
-        headers: Optional[Dict[str, str]] = None,
+        fields: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> Response:
         """Internal: Handles GET and DELETE"""
         headers = self._format_headers(headers) if headers is not None else self.headers
@@ -187,13 +187,13 @@ class HTTPClient:
         return resp
 
     @staticmethod
-    def _is_urlencoded(headers: Optional[Dict[str, str]]) -> bool:
+    def _is_urlencoded(headers: dict[str, str] | None) -> bool:
         """Determine how to encode the body"""
         if headers is not None:
             return not headers.get("content-type", "") == "application/json"
         return False
 
     @staticmethod
-    def _format_headers(headers: Dict[str, str]) -> Dict[str, str]:
+    def _format_headers(headers: dict[str, str]) -> dict[str, str]:
         """Adjust all keys to lower-case"""
         return {key.lower(): value for key, value in headers.items()}
